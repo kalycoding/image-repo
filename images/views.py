00814c85320cd9772b9bd54d1201a226db09a843
kalycoding <muhdkhalifa1@yahoo.com>
2021-09-11 17:56:37 +0100

    End point for Listing Image Category

--- images/views.py
+++ images/views.py
@@ -1,3 +1,23 @@
+from django.db.models import query
 from django.shortcuts import render
+from rest_framework import (
+    response,
+    generics,
+    permissions,
+    serializers
+)
 
+from .serializers import (
+    ImageCategorySerializer
+)
+from .models import (
+    ImageCategory,
+    Image
+)
 # Create your views here.
+
+class ImageCategoryViewList(generics.ListAPIView):
+    def get(self, request):
+        queryset = ImageCategory.objects.filter(user=request.user)  
+        serializer = ImageCategorySerializer(queryset, many=True)
+        return response.Response(serializer.data, status=200)
